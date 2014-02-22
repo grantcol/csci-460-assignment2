b8f5038dc796b039fdecff4e8843bc3bdaa89aa4
Grant Collins <grantcol@usc.edu>
2014-02-22 14:47:04 -0800

    added runnable jar and inputs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
 public class Main {
@@ -8,6 +9,8 @@ public class Main {
 	static GraphConstructor gc = new GraphConstructor("city-edges.txt");
 	static String ROOTID = "Alexandria";
 	static String GOALID = "Luxor";
+	String searchMode;
+	private static Scanner userInput;
 
 	public static void main(String [] args) throws IOException {
 		sg = gc.generateGraph();
@@ -17,6 +20,15 @@ public class Main {
 
 	public static LinkedList<Node> search(SearchGraph ss, String rootId) {
 		
+		userInput = new Scanner( System.in );
+		String searchQuery;
+		String heur;
+		System.out.print("Select an algorithm to run (greedy || astar): ");
+		searchQuery = userInput.next( );
+		System.out.println("Select a hueristic (h1 || h2): ");
+		heur = userInput.next();
+		System.out.println("sq:"+searchQuery+" heur:"+heur);
+		
 		Searcher sq = new Searcher();
 		Node root = ss.getNode(rootId);
 		root.cost = 0;
@@ -24,7 +36,6 @@ public class Main {
 		root.visited = true;
 		
 		while(!sq.empty()) {
-			
 			Node next = sq.queue.getFirst();
 			next.visited = true;
 			sq.pushStack(next);
@@ -33,10 +44,18 @@ public class Main {
 				sq.pushStack(next);
 				break;
 			}
-			//sq.queueGreedy("h1", next.expand());
-			//sq.queueGreedy("h2", next.expand());
-			sq.queueAStar("h1", next, next.expand());
-			//sq.queueAStar("h2", next, next.expand());
+			if(searchQuery.equals("greedy")) {
+				if(heur.equals("h1"))
+					sq.queueGreedy("h1", next.expand());
+				else if(heur.equals("h2"))
+					sq.queueGreedy("h2", next.expand());
+			}
+			else if(searchQuery.equals("astar")) {
+				if(heur.equals("h1"))
+					sq.queueAStar("h1", next, next.expand());
+				else if(heur.equals("h2"))
+					sq.queueAStar("h2", next, next.expand());
+			}
 			//sq.queueDFS(next.expand());
 			//sq.queueBFS(next.expand());
 			//sq.queueUCS(next, next.expand());
